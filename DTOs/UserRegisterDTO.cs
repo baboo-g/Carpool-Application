50fded55786b7ca3d6468e78c0ba6c3775deb8e9
Bilal-01 <mbilalaziz.01@gmail.com>
2023-12-03 00:42:57 +0500

    Authentication implemented

---
+++ DTOs/UserRegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace UniRideHubBackend.DTOs
+{
+	public class UserRegisterDTO
+	{
+		public string First_name { get; set; }
+		public string Last_name { get; set; }
+		public string Mobile { get; set; }
+		public string Password { get; set; }
+	}
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,19 @@
-﻿namespace UniRideHubBackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace UniRideHubBackend.Models
 {
 	public class User
 	{
 		public int Id { get; set; }
+		[Required]
 		public string First_name { get; set; }
+		[Required]
 		public string Last_name { get; set; }
+		[Required]
 		public string Mobile { get; set; }
+		[Required]
 		public string Password { get; set; }
 		public int Rides_completed { get; set; }
 //		public int Avg_rating { get; set; }
