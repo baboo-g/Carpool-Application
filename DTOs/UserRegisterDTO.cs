2e25f62d799f4400f97412dd4c14762b76e178b7
Bilal-01 <mbilalaziz.01@gmail.com>
2023-12-04 12:40:50 +0500

    JWT implemented

--- DTOs/UserRegisterDTO.cs
+++ DTOs/UserRegisterDTO.cs
@@ -2,6 +2,7 @@
 {
 	public class UserRegisterDTO
 	{
+		public int Id { get; set; }
 		public string First_name { get; set; }
 		public string Last_name { get; set; }
 		public string Mobile { get; set; }
