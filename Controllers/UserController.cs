fa02dfac2e4b84ae00ebe4f70365a696f5bb87a4
Abdullah Gohar <abdullah.gohar@gmail.com>
2024-12-03 00:19:30 +0500

    Added post method to users/

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -30,6 +30,31 @@ namespace UniRideHubBackend.Controllers
             }
         }
 
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("User data is required.");
+            }
+
+            // Validate required fields here if necessary (e.g., check if first name, last name, etc. are not null)
+
+            // Add the new user to the context
+            _context.Users.Add(user);
+
+            try
+            {
+                // Save the changes to the database
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(500, "Internal server error while adding user.");
+            }
+        }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUser(int id)
         {
